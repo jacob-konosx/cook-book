77aa6f9c11121642c34c2d02a2490faca04f542f
Meguku <41983323+Meguku@users.noreply.github.com>
2022-02-21 14:40:06 +0200

    Config added

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+	apiKey: "AIzaSyAYlyjMG6k2AZ8OS15Rysx7wiZIeiFXrOo",
+	authDomain: "cook-book-ef205.firebaseapp.com",
+	projectId: "cook-book-ef205",
+	storageBucket: "cook-book-ef205.appspot.com",
+	messagingSenderId: "463318482360",
+	appId: "1:463318482360:web:3ed7d58594755f36fb234d",
+	measurementId: "G-Z0M92P7CCT",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
